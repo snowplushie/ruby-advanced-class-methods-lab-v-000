aad0602597239e2f0126858e63d39622fee23459
Paige Wiener <snowplushie@yahoo.com>
2019-03-12 21:15:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,9 +29,7 @@ class Song
   end
   
   def self.find_by_name(song_name)
-    self.all.find do |song|
-      song.name == song_name
-      song
+    self.all.find{|song| song.name == song_name}
     end
   end
   
